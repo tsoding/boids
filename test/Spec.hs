9d2f39bf21b1d38c8b67c147088a1e3159d463b1
rexim <reximkut@gmail.com>
2016-10-25 01:16:43 +0600

    Break UTs to confirm that everything was setup right (#3)

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,7 @@ import Boids
 import Test.HUnit
 
 testInitialState :: Test
-testInitialState = TestCase (assertEqual "Initial State" (0.0, 0.0) initialState)
+testInitialState = TestCase (assertEqual "Initial State" (1.0, 0.0) initialState)
 
 main :: IO Counts
 main = runTestTT $ TestList [TestLabel "Test Initial State" testInitialState]

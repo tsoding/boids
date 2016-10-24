ba6db2e0eb45548cb57cc544ea43de9abf4ffa17
rexim <reximkut@gmail.com>
2016-10-25 01:29:22 +0600

    Exit 1 when UT fails (#3)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,13 @@
 import Boids
 import Test.HUnit
+import System.Exit
 
 testInitialState :: Test
 testInitialState = TestCase (assertEqual "Initial State" (1.0, 0.0) initialState)
 
 main :: IO Counts
-main = runTestTT $ TestList [TestLabel "Test Initial State" testInitialState]
+main = do results <- runTestTT $ TestList [TestLabel "Test Initial State" testInitialState]
+          if (errors results + failures results == 0)
+          then exitWith ExitSuccess
+          else exitWith (ExitFailure 1)
+

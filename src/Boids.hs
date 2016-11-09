45b3b4ae9640fb49dd7df5a82dbe0b9759ad5fef
rexim <reximkut@gmail.com>
2016-11-09 23:05:44 +0600

    Add base for separation rule (#10)

--- src/Boids.hs
+++ src/Boids.hs
@@ -3,17 +3,17 @@ module Boids ( World
              , initialState
              , renderState
              , nextState
-             , radsToDegrees
              ) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Geometry.Angle
 import System.Random
 import Control.Monad
 
 data World = World { worldBoids :: [Boid]
                    , worldGuide :: Point
-                   }
+                   } deriving Show
 
 data Boid = Boid { boidPosition :: Point
                  , boidHeading :: Float
@@ -22,17 +22,20 @@ data Boid = Boid { boidPosition :: Point
 
 boidsSpeed = 100.0
 guideSpeed = 100.0
+separationDistance = 50.0
 
-radsToDegrees :: Float -> Float
-radsToDegrees x = x * 180.0 / pi
+distance :: Point -> Point -> Float
+distance (x1, y1) (x2, y2) = sqrt (dx * dx + dy * dy)
+    where dx = x2 - x1
+          dy = y2 - y1
 
 renderBoid :: Boid -> Picture
-renderBoid boid = translate x y $ rotate (-heading) $ polygon ps
+renderBoid boid = translate x y $ rotate (-heading) $ pictures [circle (separationDistance / 2.0), polygon ps]
     where ps = [ (-10.0, 10.0)
                , (20.0, 0.0)
                , (-10.0, -10.0)
                ]
-          heading = radsToDegrees $ boidHeading boid
+          heading = radToDeg $ boidHeading boid
           (x, y) = boidPosition boid
 
 
@@ -42,6 +45,23 @@ guideBoidTo (guideX, guideY) boid = boid { boidSteer = da / abs da * 0.25 }
           (dx, dy) = (guideX - boidX, guideY - boidY)
           heading = boidHeading boid
           da = atan2 dy dx - boidHeading boid
+
+getNearBoids :: Boid -> Float -> [Boid] -> [Boid]
+getNearBoids boid boidDistance boids = filter isTooClose boids
+    where isTooClose boid' = distance (boidPosition boid) (boidPosition boid') <= boidDistance
+
+averageBoidsPos :: [Boid] -> Point
+averageBoidsPos = undefined
+
+separateBoid :: Boid -> [Boid] -> Boid
+separateBoid boid otherBoids = guideBoidTo guide boid
+    where nearBoids = getNearBoids boid separationDistance otherBoids
+          guide = averageBoidsPos nearBoids
+
+separationRule :: [Boid] -> [Boid]
+separationRule boids = [ separateBoid boid $ excludedBoids i | (i, boid) <- indexedBoids]
+    where indexedBoids = zip [1..] boids
+          excludedBoids i = map snd $ filter (\(j, _) -> i /= j) indexedBoids
 
 nextBoid :: Float -> Boid -> Boid
 nextBoid deltaTime boid = boid { boidPosition = ( x + deltaTime * cos heading * boidsSpeed
@@ -67,7 +87,7 @@ randomBoid = do x <- randomRIO (-100.0, 100.0)
                               }
 
 initialState :: IO World
-initialState = do boids <- replicateM 500 randomBoid
+initialState = do boids <- replicateM 100 randomBoid
                   return $ World { worldBoids = boids
                                  , worldGuide = (0.0, 0.0)
                                  }

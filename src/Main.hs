6ede5fad54748b43487df96d3acccad4856333ec
Alexey Kutepov <reximkut@gmail.com>
2016-11-09 22:12:34 +0700

    Revert "Only determined functions in 'Boids.hs'"

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,7 @@
-import System.Random (newStdGen, randomRs)
 import Graphics.Gloss
 import Boids
 
 window = InWindow "Boids" (800, 600) (10, 10)
 
-boidsCount = 100
-
-main :: IO ()
-main = do gen <- newStdGen
-          let salts = randomRs (0 :: Float, 1 :: Float) gen
-              boids = initialState boidsCount salts
+main = do boids <- initialState
           simulate window white 30 boids renderState nextState
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,26 +1,14 @@
 import Boids
-import Utils (projectToRange)
 import Test.HUnit
 import Test.HUnit.Approx
 import System.Exit
 
-
 testRadsToDegrees :: Test
 testRadsToDegrees = TestCase (assertApproxEqual "Rads to Degrees" 1e-6 180.0 (radsToDegrees pi))
 
-
-testSaltedRange :: Test
-testSaltedRange = TestCase $ do
-  assertEqual "Middle" 30 (projectToRange 0.5 (20, 40))
-  assertEqual "Minimum" 20 (projectToRange 0 (20, 40))
-  assertEqual "Maximum" 40 (projectToRange 1 (20, 40))
-
-
 main :: IO Counts
-main = do results <- runTestTT tests
-          if errors results + failures results == 0
-          then exitSuccess
-          else exitFailure
-  where tests = TestList [ TestLabel "Test Radians to Degrees conversion" testRadsToDegrees
-                         , TestLabel "Test projectToRange util function" testSaltedRange
-                         ]
+main = do results <- runTestTT $ TestList [TestLabel "Test Radians to Degrees conversion" testRadsToDegrees]
+          if (errors results + failures results == 0)
+          then exitWith ExitSuccess
+          else exitWith (ExitFailure 1)
+

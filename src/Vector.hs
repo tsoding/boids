64b913a55f5a73658632c363fd66febec9dbfecf
rexim <reximkut@gmail.com>
2017-03-27 18:42:42 +0700

    Add piece for improving the vector experience (#39)

--- src/Vector.hs
+++ src/Vector.hs
@@ -6,5 +6,9 @@ import Data.List
 fromPoints :: Point -> Point -> Vector
 fromPoints (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
 
+-- TODO(dfec87ca-77f5-4ad6-a015-81b49929a5a1): Improve Vector experience
+--
+-- This application works with vectors pretty extensively, and
+-- functions like `addTwoVectors` are not really convenient.
 addTwoVectors :: Vector -> Vector -> Vector
 addTwoVectors (x1, y1) (x2, y2) = (x2 + x1, y2 + y1)
